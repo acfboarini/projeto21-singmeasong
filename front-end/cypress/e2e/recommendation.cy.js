e5cc7834d7eb0811e75296e1edd80dbbe5b290e3
Augusto <acfb.app@gmail.com>
2022-11-10 04:32:59 -0300

    removendo qualquer inutilidade e organizando a parta de teste e2e

--- front-end/cypress/e2e/recommendation.cy.js
+++ front-end/cypress/e2e/recommendation.cy.js
@@ -1,13 +1,9 @@
-/* eslint-disable no-undef */
 /// <reference types="cypress" />
 
-//import { faker } from "@faker-js/faker";
-
-beforeEach(() => {
-    cy.request("POST", "http://localhost:5000/reset", {}); 
-})
-
 describe("testing e2e", () => {
+    beforeEach(() => {
+        cy.request("POST", "http://localhost:5000/reset", {}); 
+    })
 
     it ("testing initial page", () => {
         cy.visit("http://localhost:3000");
